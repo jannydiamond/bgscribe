93ed7d4438df7c8a2f5ab1c8a4e89a9839900831
Theo Salzmann <theo.salzmann@sandstorm.de>
2021-03-21 15:54:08 +0100

    FEATURE: Sort gameachievements by set

--- src/pages/Games/GameDetails/GameAchievements/index.tsx
+++ src/pages/Games/GameDetails/GameAchievements/index.tsx
@@ -1,11 +1,12 @@
 import AchievementUnlockTile from 'components/AchievementUnlockTile'
 import FloatingButton from 'components/FloatingButton'
+import H2 from 'components/__styled__/H2'
 import P from 'components/__styled__/P'
 import { useModal } from 'hooks/useModal'
 import ListItem from 'pages/Games/__styled__/ListItem'
 import React, { useState } from 'react'
 import { useSelector } from 'react-redux'
-import { selectAchievementsByGame } from 'Redux/root'
+import { selectSetAchievementsByIdByGame } from 'Redux/root'
 import { RootState } from 'Redux/store'
 import { Achievement, GameAchievement } from 'types'
 import AddAchievementsModal from './AddAchievementsModal'
@@ -19,32 +20,45 @@ const GameAchievements = (props: Props) => {
   const addAchievementsModal = useModal()
   const unlockAchievementModal = useModal()
 
-  const achievements = useSelector((state: RootState) =>
-    selectAchievementsByGame(state, { gameId: props.gameId })
+  const achievementsBySet = Object.values(
+    useSelector((state: RootState) =>
+      selectSetAchievementsByIdByGame(state, { gameId: props.gameId })
+    )
   )
 
-  const [achievementToUnlock, setAchievementToUnlock] = useState<GameAchievement & Achievement | null>(null)
+  const [achievementToUnlock, setAchievementToUnlock] = useState<
+    (GameAchievement & Achievement) | null
+  >(null)
 
-  const handleAchievementToUnlockClick = (achievement: GameAchievement & Achievement) => {
+  const handleAchievementToUnlockClick = (
+    achievement: GameAchievement & Achievement
+  ) => {
     setAchievementToUnlock(achievement)
     unlockAchievementModal.show()
   }
 
   return (
     <>
-      {achievements.length > 0 ? (
-        achievements.map((achievement) => (
-          <ListItem key={achievement.id}>
-            <AchievementUnlockTile
-              imageSrc={achievement.image ? achievement.image : ''}
-              title={achievement.title}
-              subtitle={achievement.description}
-              level={achievement.level}
-              onClick={() => handleAchievementToUnlockClick(achievement)}
-              unlocked={achievement.achieved}
-            />
-          </ListItem>
-        ))
+      {achievementsBySet.length > 0 ? (
+        achievementsBySet.map((set) => {
+          return (
+            <>
+              <H2>{set.title}</H2>
+              {set.achievements.map((achievement) => (
+                <ListItem key={achievement.id}>
+                  <AchievementUnlockTile
+                    imageSrc={achievement.image ? achievement.image : ''}
+                    title={achievement.title}
+                    subtitle={achievement.description}
+                    level={achievement.level}
+                    onClick={() => handleAchievementToUnlockClick(achievement)}
+                    unlocked={achievement.achieved}
+                  />
+                </ListItem>
+              ))}
+            </>
+          )
+        })
       ) : (
         <P>No achievements have been added, yet.</P>
       )}
