f3f867883137d6525a9633f8f3df01d28add142c
Janett Salzmann <J.Hickel@t-systems.com>
2021-03-21 12:30:49 +0100

    FIX: Overflow and image issue in tiles

--- src/components/AchievementAvatar/__styled__/Wrapper.tsx
+++ src/components/AchievementAvatar/__styled__/Wrapper.tsx
@@ -9,6 +9,7 @@ type Props = {
 const Wrapper = styled('div')<Props>`
   height: 64px;
   width: 64px;
+  flex: 0 0 auto;
   overflow: hidden;
   border-radius: 4px;
   border: 4px solid ${props => props.unlocked ? props.theme.achievement.level[props.level].unlocked : props.theme.achievement.level[props.level].locked};
